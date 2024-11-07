dc10493c4bcdf19d663e327defd745481820d063
ryanspeulstra <129810204+ryanspeulstra@users.noreply.github.com>
2024-11-06 19:41:42 -0500

    Update form.js

--- form.js
+++ form.js
@@ -1,16 +1,29 @@
-function displayRentalRate() {
-    var inputDate = $('#startdate').val();
-    var discount = $('input[name=options]:checked', '#discountCheck').val();
-    var insurance = $("input[id=gridCheck]:checked").val();
-    var numberOfDrivers = $("#numberdrivers").val();
-    var numberOfDays= $("#numberdays").val();
-    var driverRate = 0;
+$(document).ready(function () {
+    function displayRentalRate() {
+        var numberOfDrivers = $("#numberdrivers").val();
+        var numberOfDays = $("#numberdays").val();
+        var driverRate = 0;
+        $('#model').on("change", calculateDailyRate);
+        $('#insurance').on("change", needsInsurance);
+        $('#numberdrivers').on("change", calculateDriverRate);
+        $('#startdate').on("change", isValidDate);
 
+        var discount = $('input[name=options]:checked', '#discountCheck').val();
+        $('#discount').html("Discount Rate: " + discount * 100);
 
-$('#model').change(function() {
+        var costPerDay = (rentalRate - (rentalRate * discount) + insuranceRate + driverRate)
+        $('#bydaycost').html("Cost per day: " + costPerDay);
+
+        var totalCost = (costPerDay * numberOfDays);
+        $('#totalcost').html("Total Cost: " + totalCost);
+
+    }
+});
+
+function calculateDailyRate() {
     var modelChoice = $('#model :selected').val();
     switch (modelChoice) {
-        case "SUV: Honda CR-V" :
+        case "SUV: Honda CR-V":
         case "Luxury Car: Tesla 3":
             var rentalRate = 50;
             break;
@@ -27,41 +40,33 @@ $('#model').change(function() {
             break;
     }
     $('#dailyrate').html("Daily Rate: " + rentalRate);
-});
+}
 
-
-
-    $('#discount').html("Discount Rate: " + discount * 100);
-    $('#totaldriversoutput').html("Drivers: " + numberOfDrivers);
-
+function needsInsurance() {
+    var insurance = $("input[id=gridCheck]:checked").val();
     if (insurance == "on") {
-       var insuranceRate = 10
-       $('#insurance').html("Insurance: " + insuranceRate);
+        var insuranceRate = 10
+        $('#insurance').html("Insurance: " + insuranceRate);
+    } else {
+        var insuranceRate = 0;
     }
+}
 
+function calculateDriverRate() {
+    if (numberOfDrivers > 1) {
+        var driverRate = numberOfDrivers * 15;
+    }
+    $('#totaldriversoutput').html("Drivers: " + numberOfDrivers);
+}
+
+function isValidDate() {
+    var inputDate = $('#startdate').val();
     var currentDate = new Date();
     var startDate = new Date(inputDate);
     if (currentDate > startDate) {
         alert("Error: Date is prior to current date.");
-        $('#rentalForm').on("submit", function(event) {
+        $('#rentalForm').on("submit", function (event) {
             event.preventDefault();
         });
     }
-
-    console.log(startDate);
-    console.log(currentDate);
-
-
-    if (numberOfDrivers > 1) {
-    var driverRate = numberOfDrivers * 15;
-    }
-
-    var costPerDay = (rentalRate - (rentalRate * discount) + insuranceRate + driverRate)
-    $('#bydaycost').html("Cost per day: " + costPerDay);
- 
-
-    var totalCost = (costPerDay * numberOfDays);
-    $('#totalcost').html("Total Cost: " + totalCost);
-
 }
-
